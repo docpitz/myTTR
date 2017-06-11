1a0091821b1bbb9ec9f81b784446f6b62f556281
chokdee <ju.melzer@gmail.com>
2017-06-11 13:36:47 +0200

    prepare next season

--- myTTR/src/main/java/com/jmelzer/myttr/model/Saison.java
+++ myTTR/src/main/java/com/jmelzer/myttr/model/Saison.java
@@ -12,10 +12,12 @@ public enum Saison {
     SAISON_2015("14/15"),
     SAISON_2016("15/16"),
     SAISON_2017("16/17"),
-    SAISON_2018("17/18");
+    SAISON_2018("17/18"),
+    SAISON_2019("18/19");
 
     public static List<String> saisons = new ArrayList<>();
     static {
+        saisons.add(SAISON_2018.getName());
         saisons.add(SAISON_2017.getName());
         saisons.add(SAISON_2016.getName());
         saisons.add(SAISON_2015.getName());
