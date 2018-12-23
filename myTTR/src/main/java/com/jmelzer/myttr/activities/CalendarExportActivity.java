fd5e2f025eba299b4686e6c607a70a4a9671bcb9
chokdee <ju.melzer@gmail.com>
2018-12-23 19:55:36 +0100

    80 % calendar version

--- myTTR/src/main/java/com/jmelzer/myttr/activities/CalendarExportActivity.java
+++ myTTR/src/main/java/com/jmelzer/myttr/activities/CalendarExportActivity.java
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -13,20 +14,32 @@ import android.provider.CalendarContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jmelzer.myttr.Constants;
 import com.jmelzer.myttr.Mannschaftspiel;
+import com.jmelzer.myttr.R;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static com.jmelzer.myttr.MyApplication.selectedMannschaft;
 
 public class CalendarExportActivity extends BaseActivity {
+
+    @SuppressLint("SimpleDateFormat")
+    SimpleDateFormat format = new SimpleDateFormat("dd.MM.yy HH:mm");
 
     // Projection array. Creating indices for this array instead of doing
 // dynamic lookups improves performance.
@@ -51,49 +64,24 @@ public class CalendarExportActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (toLoginIfNeccassry()) {
+            return;
+        }
 
-        Cursor cur = null;
-        ContentResolver cr = getContentResolver();
-        Uri uri = CalendarContract.Calendars.CONTENT_URI;
-        String selection = CalendarContract.Calendars.VISIBLE + " = 1 AND " + CalendarContract.Calendars.IS_PRIMARY + "=1";
+        setContentView(R.layout.calendar);
 
 
 //        checkPermissions(42, Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR);
 // Submit the query and get a Cursor object back.
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Log.d(Constants.LOG_TAG, "-------------------");
-        cur = cr.query(uri, EVENT_PROJECTION, selection, null, null);
-        long calID = 0;
-        if (cur.moveToNext()) {
-            String displayName = null;
-            String accountName = null;
-            String ownerName = null;
 
-            // Get the field values
-            calID = cur.getLong(PROJECTION_ID_INDEX);
-            displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
-            accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
-            ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
+//
+//        int counter = 0;
 
-            // Do something with the values...
-            String calendarInfo = String.format("Calendar ID: %s\nDisplay Name: %s\nAccount Name: %s\nOwner Name: %s", calID, displayName, accountName, ownerName);
+//        if ( calID > 0 ) {
 
-            Log.d(Constants.LOG_TAG, calendarInfo);
-
-        }
-        @SuppressLint("SimpleDateFormat")
-        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yy HH:mm");
         Date now = new Date();
-        int counter = 0;
+        List<Mannschaftspiel> list = new ArrayList<>();
+
         for (Mannschaftspiel mannschaftspiel : selectedMannschaft.getSpiele()) {
             try {
                 Date d = format.parse(mannschaftspiel.getDate());
@@ -105,44 +93,38 @@ public class CalendarExportActivity extends BaseActivity {
                     String title = String.format("%s - %s",
                             mannschaftspiel.getHeimMannschaft().getName(),
                             mannschaftspiel.getGastMannschaft().getName());
-                    if (!isEventAlreadyExist(title)) {
-                        ContentValues values = createEntry(calID, d.getTime(), cal.getTimeInMillis(), title, title);
-                        cr.insert(CalendarContract.Events.CONTENT_URI, values);
-                        Log.d(Constants.LOG_TAG, "created=" + values);
-                        counter++;
-                    } else {
-
-                        Log.d(Constants.LOG_TAG, "event exists " + title);
-                    }
+//                        if (!isEventAlreadyExist(title)) {
+//                            ContentValues values = createEntry(calID, d.getTime(), cal.getTimeInMillis(), title, title);
+//                            cr.insert(CalendarContract.Events.CONTENT_URI, values);
+//                            Log.d(Constants.LOG_TAG, "created=" + values);
+//                            counter++;
+//                        } else {
+//
+//                            Log.d(Constants.LOG_TAG, "event exists " + title);
+//                        }
+                    list.add(mannschaftspiel);
                 }
             } catch (ParseException e) {
-                e.printStackTrace();
+                Log.e(Constants.LOG_TAG, e.getMessage());
             }
         }
 
+        ListView listview = findViewById(R.id.cal_list);
+        CalendarRowAdapter adapter = new CalendarRowAdapter(this,
+                android.R.layout.simple_list_item_1,
+                list);
+        listview.setAdapter(adapter);
 
-//        uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
-//        long eventID = Long.parseLong(uri.getLastPathSegment());
-//        Log.i(Constants.LOG_TAG, "Event Created, the event id is: " + eventID);
-        Toast.makeText(this, counter + " Termine wurden eingetragen", Toast.LENGTH_SHORT).show();
-        Log.d(Constants.LOG_TAG, "-------------------");
+
     }
 
-    private boolean isEventAlreadyExist(String eventTitle) {
+    private boolean isEventAlreadyExist(String eventTitle, long startMillis, long endMillis) {
         final String[] INSTANCE_PROJECTION = new String[]{
                 CalendarContract.Instances.EVENT_ID,      // 0
                 CalendarContract.Instances.BEGIN,         // 1
                 CalendarContract.Instances.TITLE          // 2
         };
 
-        long startMillis = 0;
-        long endMillis = 0;
-        Calendar beginTime = Calendar.getInstance();
-        beginTime.set(2017, 11, 15, 6, 00);
-        startMillis = beginTime.getTimeInMillis();
-        Calendar endTime = Calendar.getInstance();
-        endTime.set(2017, 11, 15, 8, 00);
-        endMillis = endTime.getTimeInMillis();
 
         // The ID of the recurring event whose instances you are searching for in the Instances table
         String selection = CalendarContract.Instances.TITLE + " = ?";
@@ -179,5 +161,123 @@ public class CalendarExportActivity extends BaseActivity {
 
         if (!permissions)
             ActivityCompat.requestPermissions(this, permissionsId, callbackId);
+    }
+
+    public void addAll(View view) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(this, "Keine ausreichenden Berechtigungen zum Eintragen",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        Log.d(Constants.LOG_TAG, "-------------------");
+
+        ContentResolver cr = getContentResolver();
+        Uri uri = CalendarContract.Calendars.CONTENT_URI;
+        String selection = CalendarContract.Calendars.VISIBLE + " = 1 AND " + CalendarContract.Calendars.IS_PRIMARY + "=1";
+        Cursor cur = cr.query(uri, EVENT_PROJECTION, selection, null, null);
+
+        long calID = 0;
+        if (cur != null && cur.moveToNext()) {
+            String displayName = null;
+            String accountName = null;
+            String ownerName = null;
+
+            // Get the field values
+            calID = cur.getLong(PROJECTION_ID_INDEX);
+            displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
+            accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+            ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
+
+            // Do something with the values...
+            String calendarInfo = String.format("Calendar ID: %s\nDisplay Name: %s\nAccount Name: %s\nOwner Name: %s", calID, displayName, accountName, ownerName);
+
+            Log.d(Constants.LOG_TAG, calendarInfo);
+            cur.close();
+
+        }
+        Date now = new Date();
+        int counter = 0;
+        int exists = 0;
+        for (Mannschaftspiel mannschaftspiel : selectedMannschaft.getSpiele()) {
+            try {
+                Date d = format.parse(mannschaftspiel.getDate());
+                if (now.getTime() < d.getTime()) {
+                    Log.d(Constants.LOG_TAG, "Date=" + d);
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(d);
+                    cal.add(Calendar.HOUR, 3); // 3h
+                    String title = String.format("%s - %s",
+                            mannschaftspiel.getHeimMannschaft().getName(),
+                            mannschaftspiel.getGastMannschaft().getName());
+                    if (!isEventAlreadyExist(title, d.getTime(), cal.getTimeInMillis())) {
+                        ContentValues values = createEntry(calID, d.getTime(), cal.getTimeInMillis(), title, title);
+                        cr.insert(CalendarContract.Events.CONTENT_URI, values);
+                        Log.d(Constants.LOG_TAG, "created=" + values);
+                        counter++;
+                    } else {
+                        exists++;
+                        Log.d(Constants.LOG_TAG, "event exists " + title);
+                    }
+                }
+                if (counter > 0 || exists > 0) break;
+            } catch (ParseException e) {
+                Log.e(Constants.LOG_TAG, e.getMessage());
+            }
+
+        }
+
+        Toast.makeText(this,
+                "Es wurden " + counter + " Einträge in den Kalender geschrieben\n" +
+                exists + " Einträge existierten bereits",
+                Toast.LENGTH_LONG).show();
+
+    }
+
+    private static class ViewHolder {
+        TextView date;
+        TextView title;
+    }
+
+    class CalendarRowAdapter extends ArrayAdapter<Mannschaftspiel> {
+        private LayoutInflater layoutInflater;
+
+        public CalendarRowAdapter(Context context, int resource, List<Mannschaftspiel> list) {
+            super(context, resource, list);
+            layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+
+            if (convertView == null) {
+                convertView = layoutInflater.inflate(R.layout.calendar_row, parent, false);
+                holder = new ViewHolder();
+                holder.date = convertView.findViewById(R.id.cal_date);
+                holder.title = (TextView) convertView.findViewById(R.id.cal_name);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Mannschaftspiel item = getItem(position);
+            holder.date.setText(item.getDate());
+            String title = String.format("%s - %s",
+                    item.getHeimMannschaft().getName(),
+                    item.getGastMannschaft().getName());
+//
+            holder.title.setText(title);
+
+            return convertView;
+        }
     }
 }
